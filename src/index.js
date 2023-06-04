e0195aab5b65ffb2eabac427df5c4d0548429934
Douglas Izquierdo Armenteros <108787517+leftDoug@users.noreply.github.com>
2023-06-03 21:17:32 -0400

    aplicacion dividida en componentes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const DATA = [
+  {id: 'todo-0', name: 'Eat', completed: true},
+  {id: 'todo-1', name: 'Sleep', completed: false},
+  {id: 'todo-2', name: 'Repeat', completed: false}
+];
+const BUTTONS = [
+  {id: 'btn-0', name: 'all'},
+  {id: 'btn-1', name: 'Active'},
+  {id: 'btn-2', name: 'Completed'}
+];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App subject='Clarice' />
+    <App tasks={DATA} buttons={BUTTONS} />
   </React.StrictMode>
 );
